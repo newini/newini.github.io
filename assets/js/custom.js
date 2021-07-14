473faa17f88ac67b06d4a4d7a0353d72a7ef3b03
Eunchong Kim <chariskimec@gmail.com>
2021-07-14 10:18:10 +0900

    Added navbar change bkg color on scroll

---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+/* Change navbar bkg color On Scroll */
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.background = "#201e27";
+  } else {
+    document.getElementById("navbar").style.background = "none";
+  }
+}
