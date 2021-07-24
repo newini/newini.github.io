c862ef900f564e3ba7453839391f399d7f679ee1
Eunchong Kim <chariskimec@gmail.com>
2021-07-24 12:29:27 +0900

    Added js chagne language

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,27 @@
+/* Get parameter from url */
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+let locale = urlParams.get('locale')
+if (!locale || (locale != 'en' && locale != 'ja' && locale != 'ko') ) {
+  locale = 'en';
+}
+
+/* Language */
+const language = {
+  en: {
+    home_message: "Welcome to Eunchong Kim's Home Page!"
+  },
+  ja: {
+    home_message: "金恩寵のホームページへようこそ"
+  },
+  ko: {
+    home_message: "김은총의 홈페이지"
+  }
+}
+document.getElementById('home_message').innerHTML = language[locale]['home_message'];
+
+
+/* Home */
 /* Change navbar bkg color on Scroll */
 window.onscroll = function() { scrollFunction() };
 
