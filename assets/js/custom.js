24b2087ca1e82c498a7938a39fc5aa6531f944a3
Eunchong Kim <chariskimec@gmail.com>
2021-07-24 12:29:54 +0900

    Changaed navbar behavior on scroll

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -27,8 +27,8 @@ window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.background = "#201e27";
+    document.getElementById("navbar").classList.add('navbar-scroll');
   } else {
-    document.getElementById("navbar").style.background = "none";
+    document.getElementById("navbar").classList.remove('navbar-scroll');
   }
 }
